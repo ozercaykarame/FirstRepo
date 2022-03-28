1aa0c15cf3768c02f9dc720bc5486b34518087cf
Ozer Caykara <102478020+ozercaykarame@users.noreply.github.com>
2022-03-28 23:16:56 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,12 +1,12 @@
 using System;
 
 namespace FirstRepo
-{
-  internal class Program
-  {
-  static void Main(string[] args)
-  {
-    Console.WriteLine("Hello World!");
-  }
-  }
+	{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
