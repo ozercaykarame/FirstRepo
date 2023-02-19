12555dec3852db4b6d8889c61e60f29991088d80
codeflipper <109942661+codeflipper@users.noreply.github.com>
2023-02-19 20:48:20 +0300

    Format correction

--- src/OMath.cs
+++ src/OMath.cs
@@ -1,20 +1,20 @@
 public class OMath : IOMath
 {
-  public int Fact(int number)
-  {
-    if(number > 1)
-      return number * Fact(number -1)
-        else
-          return 1;
-  }
-  
-  public int FactWithLoop(int number)
-  {
-    int result=1;
-    for(int i=1;i<=number;i++)
+    public int Fact(int number)
     {
-      result = result * i;
+        if (number > 1)
+            return number * Fact(number - 1)
+            else
+            return 1;
     }
-    return result;
-  }
+
+    public int FactWithLoop(int number)
+    {
+        int result = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            result = result * i;
+        }
+        return result;
+    }
 }
